2b6b4dff336e2233e92f385cbc427ed022d196ac
oespino <oespino88@gmail.com>
2022-08-27 21:23:56 +0200

    Add share icon

--- components/Detail.jsx
+++ components/Detail.jsx
@@ -2,9 +2,23 @@ import Image from 'next/image'
 
 export default function Detail({ id, title, date, category, image, contentHtml }) {
 
+    function handleClick() {
+        if (navigator.share) {
+            navigator.share({
+                title: title,
+                url: window.location.href
+            })
+        }
+    }
+
     return (
         <div>
-            <h3 className="text-4xl font-bold pb-8">{title}</h3>
+            <div className='flex justify-between'>
+                <h3 className="text-4xl font-bold pb-8">{title}</h3>
+                <div className="text-right fill-cyan-700 mx-4">
+                    <a href="#"><Image src={`/share_icon.svg`} layout="fixed" height={50} width={50} onClick={() => handleClick()}></Image></a>
+                </div>
+            </div>
             <Image src={`/images/${image}`} height={300} width={900} layout="responsive" objectFit="cover"></Image>
             <div className="py-4" dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </div>
